93d7af80fb1654b7346561ce0b97fe9faf60eb75
Rosarial <rosarialdeveloper@gmail.com>
2024-09-23 17:34:46 -0300

    Update config.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,5 +2,5 @@ import dotenv from 'dotenv';
 dotenv.config();
 export const JWT_SECRET = process.env.JWT_SECRET || 'defaultsecret';
 export const DATABASE_URL =
-  process.env.DATABASE_URL || 'mysql://andre:rds20120243@localhost:3306/andre';
+  process.env.DATABASE_URL || 'mysql://u940267718_rosariall:Lo110720@193.203.166.107:3306/u940267718_rosariall';
 export const PORT_DEFAULT = process.env.PORT || 3000;
